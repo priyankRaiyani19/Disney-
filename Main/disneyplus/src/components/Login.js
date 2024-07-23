9139f6dd41ad65f27dd120be3000da8d8fc88f65
priyank <priyankraiyani19@gmail.com>
2024-07-23 22:15:53 +0530

    Router commit

--- Main/disneyplus/src/components/Login.js
+++ Main/disneyplus/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LoginImg from '../assents/login-background.jpg';
 
 const Login = (props) => {
     return (
@@ -6,7 +7,7 @@ const Login = (props) => {
             <div className=" flex flex-col justify-center items-center pt-[80px] pr-[40px]
             text-white text-[50px] mb-[10vw] w-[100%] h-[100%]
              relative min-h-[100vh] box-border">
-                Hello World!
+                <LoginImg/>
             </div>
         </div>
     );
