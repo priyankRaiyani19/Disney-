df771f31ba412fb68b22dc34f3859958778db432
priyank <priyankraiyani19@gmail.com>
2024-07-23 21:46:58 +0530

    Router commit

--- Main/disneyplus/src/App.js
+++ Main/disneyplus/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 const App = () => {
     return (
-        <div>
-
-          
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Login/>} />
+                </Routes>
+            </Router>
         </div>
     );
 };
---
+++ Main/disneyplus/src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = (props) => {
+    return (
+        <div className="overflow-hidden flex flex-col text-center h-[100vh]">
+            <div className=" flex flex-col justify-center items-center pt-[80px] pr-[40px]
+            text-white text-[50px] mb-[10vw] w-[100%] h-[100%]
+             relative min-h-[100vh] box-border">
+                Hello World!
+            </div>
+        </div>
+    );
+};
+
+export default Login;
